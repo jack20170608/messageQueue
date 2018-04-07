cf9b878f758f94d0c8ddbea99a5072f37d8fd0fd
Jack <275287908@qq.com>
2018-04-07 12:01:40 +0800

    initial check in

---
+++ activeMq/helloActiveMq/src/main/java/com/fangming/mq/activeMq/App.java
@@ -0,0 +1,13 @@
+package com.fangming.mq.activeMq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
